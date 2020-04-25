c61566dcc305d77aef1004c5890f6c5ec662b924
David Nesterov-Rappoport <davisha999@gmail.com>
2020-04-24 21:49:23 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/covid19preventionapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid19preventionapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+}
+
+class MyHomePage extends StatefulWidget {
+
+}
+

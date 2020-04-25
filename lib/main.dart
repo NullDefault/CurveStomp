0cbee2640ecea60ba416ff61b8687baa60361ab1
NullDefault <davisha999@gmail.com>
2020-04-24 23:02:13 -0400

    Added placeholder main so the app can be ran at this point

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Main());
 }
 
-class MyApp extends StatelessWidget {
+class Main extends StatelessWidget {
+  final appTitle = 'Covid-19 Local Spread Prevention App';
 
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: appTitle,
+      initialRoute: 'placeholder',
+      routes: {
+        'placeholder': (context) => PlaceHolder(),
+      },
+    );
+  }
 }
 
-class MyHomePage extends StatefulWidget {
+class PlaceHolder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "placeholder",
+      home: new Scaffold(
+        backgroundColor: Color.fromRGBO(51, 51, 51, 1),
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return MediaQuery
+                .of(context)
+                .orientation == Orientation.portrait
+                ? _buildNarrowLayout(context)
+                : _buildWideLayout(context);
+          },
+        ),
+      ),
+    );
+  }
 
+  Widget _buildNarrowLayout(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Image(
+              image: AssetImage("assets/virus_icon.png"),
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            "CurveFlatteningApp",
+            style: TextStyle(
+                fontSize: 28,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildWideLayout(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Image(
+              image: AssetImage("assets/virus_icon.png"),
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            "CurveFlatteningApp",
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.teal,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
 

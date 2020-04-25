4c705ea39bf68706c90a5226956694aa8e0ee6e2
NullDefault <davisha999@gmail.com>
2020-04-25 12:56:55 -0400

    Added buttons to wide orientation of landing page and also added GetFlutter to pubspec dependencies

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
 
-
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,9 +10,7 @@ class LandingPage extends StatelessWidget {
         backgroundColor: Color.fromRGBO(51, 51, 51, 1),
         body: OrientationBuilder(
           builder: (context, orientation) {
-            return MediaQuery
-                .of(context)
-                .orientation == Orientation.portrait
+            return MediaQuery.of(context).orientation == Orientation.portrait
                 ? _buildNarrowLayout(context)
                 : _buildWideLayout(context);
           },
@@ -39,28 +36,25 @@ class LandingPage extends StatelessWidget {
           child: Text(
             "CurveFlatteningApp.",
             style: TextStyle(
-                fontSize: 28,
-                color: Colors.teal,
-                fontWeight: FontWeight.bold
-            ),
+                fontSize: 28, color: Colors.teal, fontWeight: FontWeight.bold),
           ),
         ),
         Center(
           child: Padding(
             padding: const EdgeInsets.only(top: 12.0),
             child: GFButton(
-                onPressed: (){},
-                text: "Symptom Tracker",
-                icon: Icon(Icons.healing),
-                shape: GFButtonShape.pills,
-                color: Colors.greenAccent,
-                textColor: Colors.black,
+              onPressed: () {},
+              text: "Symptom Tracker",
+              icon: Icon(Icons.healing),
+              shape: GFButtonShape.pills,
+              color: Colors.greenAccent,
+              textColor: Colors.black,
             ),
           ),
         ),
         Center(
           child: GFButton(
-            onPressed: (){},
+            onPressed: () {},
             text: "Exposure Tracking",
             icon: Icon(Icons.record_voice_over),
             shape: GFButtonShape.pills,
@@ -70,7 +64,7 @@ class LandingPage extends StatelessWidget {
         ),
         Center(
           child: GFButton(
-            onPressed: (){},
+            onPressed: () {},
             text: "General Guidelines",
             icon: Icon(Icons.spa),
             shape: GFButtonShape.pills,
@@ -83,31 +77,72 @@ class LandingPage extends StatelessWidget {
   }
 
   Widget _buildWideLayout(BuildContext context) {
-    return Column(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Container(
-              width: 200,
-              height: 200,
-              child: Image(
-                image: AssetImage("assets/virus_icon.png"),
+        Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    width: 180,
+                    height: 180,
+                    child: Image(
+                      image: AssetImage("assets/virus_icon.png"),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "CurveFlatteningApp.",
+                  style: TextStyle(
+                      fontSize: 18, color: Colors.teal, fontWeight: FontWeight.bold),
+                ),
+              )
+        ]),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: GFButton(
+                  onPressed: () {},
+                  text: "Symptom Tracker",
+                  icon: Icon(Icons.healing),
+                  shape: GFButtonShape.pills,
+                  color: Colors.greenAccent,
+                  textColor: Colors.black,
+                ),
               ),
             ),
-          ),
-        ),
-        Center(
-          child: Text(
-            "CurveFlatteningApp",
-            style: TextStyle(
-                fontSize: 28,
-                color: Colors.teal,
-                fontWeight: FontWeight.bold
+            Center(
+              child: GFButton(
+                onPressed: () {},
+                text: "Exposure Tracking",
+                icon: Icon(Icons.record_voice_over),
+                shape: GFButtonShape.pills,
+                color: Colors.greenAccent,
+                textColor: Colors.black,
+              ),
             ),
-          ),
-        )
+            Center(
+              child: GFButton(
+                onPressed: () {},
+                text: "General Guidelines",
+                icon: Icon(Icons.spa),
+                shape: GFButtonShape.pills,
+                color: Colors.greenAccent,
+                textColor: Colors.black,
+              ),
+            )
+          ],
+        ),
       ],
     );
   }

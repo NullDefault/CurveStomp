413dd6b7aa78544a65fe9d62835ebe8027f7be90
NullDefault <davisha999@gmail.com>
2020-04-26 00:20:11 -0400

    Updated landing page to make it more pretty and have the decided upon app name

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
 
+import 'animated_wave.dart';
 import 'exposure_tracker.dart';
 import 'general_guidelines.dart';
 import 'symptom_tracker.dart';
@@ -39,67 +40,88 @@ class LandingPage extends StatelessWidget {
   }
 
   Widget _buildNarrowLayout(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
       children: <Widget>[
-        Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            child: Image(
-              image: AssetImage("assets/virus_icon.png"),
+        Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Image(
+                image: AssetImage("assets/virus_icon.png"),
+              ),
             ),
           ),
-        ),
-        Center(
-          child: Text(
-            "CurveFlatteningApp.",
-            style: TextStyle(
-                fontSize: 28,
-                color: Colors.greenAccent,
-                fontWeight: FontWeight.bold),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                "CurveStomp",
+                style: TextStyle(
+                    fontSize: 38,
+                    color: Colors.greenAccent[200],
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 12.0),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: GFButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "SymptomTracker");
+                },
+                text: "Symptom Tracker",
+                icon: Icon(Icons.healing),
+                shape: GFButtonShape.pills,
+                color: Colors.greenAccent,
+                textColor: Colors.black,
+              ),
+            ),
+          ),
+          Center(
             child: GFButton(
               onPressed: () {
-                Navigator.pushNamed(context, "SymptomTracker");
+                Navigator.pushNamed(context, "ExposureTracker");
               },
-              text: "Symptom Tracker",
-              icon: Icon(Icons.healing),
+              text: "Exposure Tracking",
+              icon: Icon(Icons.record_voice_over),
               shape: GFButtonShape.pills,
               color: Colors.greenAccent,
               textColor: Colors.black,
             ),
           ),
-        ),
-        Center(
-          child: GFButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "ExposureTracker");
-            },
-            text: "Exposure Tracking",
-            icon: Icon(Icons.record_voice_over),
-            shape: GFButtonShape.pills,
-            color: Colors.greenAccent,
-            textColor: Colors.black,
+          Center(
+            child: GFButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "GeneralGuidelines");
+              },
+              text: "General Guidelines",
+              icon: Icon(Icons.spa),
+              shape: GFButtonShape.pills,
+              color: Colors.greenAccent,
+              textColor: Colors.black,
+            ),
+          ),
+        ],
+      ),
+        onBottom(
+          AnimatedWave(
+            height: 65,
+            speed: 0.55,
+            color: Color.fromRGBO(77, 235, 219, 0.2),
           ),
         ),
-        Center(
-          child: GFButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "GeneralGuidelines");
-            },
-            text: "General Guidelines",
-            icon: Icon(Icons.spa),
-            shape: GFButtonShape.pills,
-            color: Colors.greenAccent,
-            textColor: Colors.black,
+        onBottom(
+          AnimatedWave(
+            height: 45,
+            speed: 0.63,
+            color: Color.fromRGBO(131, 230, 247, 0.2),
           ),
         ),
-      ],
+    ]
     );
   }
 
@@ -180,4 +202,11 @@ class LandingPage extends StatelessWidget {
       ],
     );
   }
+  onBottom(Widget child) => Positioned.fill(
+    // This properly places the waves within the stack
+    child: Align(
+      alignment: Alignment.bottomCenter,
+      child: child,
+    ),
+  );
 }

7efd895606339df3127ce4379b6c29c06b02f71c
NullDefault <davisha999@gmail.com>
2020-04-27 16:12:51 -0400

    Refactored general_guidelines

--- lib/general_guidelines.dart
+++ lib/general_guidelines.dart
@@ -4,18 +4,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:getflutter/components/card/gf_card.dart';
 import 'package:getflutter/getflutter.dart';
+
 import 'Guidelines/COVIDTesting.dart';
 import 'Guidelines/aboutCovid19.dart';
 import 'Guidelines/otherGuidelines.dart';
 import 'Guidelines/whatToDoToAvoid.dart';
 
-
-class GeneralGuidelines extends StatefulWidget{
+class GeneralGuidelines extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => GeneralGuidelinesState();
 }
 
-class GeneralGuidelinesState extends State<GeneralGuidelines> with TickerProviderStateMixin {
+class GeneralGuidelinesState extends State<GeneralGuidelines>
+    with TickerProviderStateMixin {
+  Widget guidelineCard(String titleText, String infoText, targetPage) {
+    return GFCard(
+      color: Colors.white70,
+      title: GFListTile(
+          title: Text(
+            titleText,
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 22, color: Colors.blue),
+          ),
+          icon: GFIconButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => targetPage));
+            },
+            icon: Icon(Icons.arrow_forward),
+          )),
+      content: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Text(infoText),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,94 +77,23 @@ class GeneralGuidelinesState extends State<GeneralGuidelines> with TickerProvide
           child: ListView(
               padding: new EdgeInsets.only(left: 2, right: 2, bottom: 2),
               children: <Widget>[
-                GFCard(
-                  color: Colors.white70,
-                  title: GFListTile(
-                      title: Text(
-                        'About COVID-19',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22, color: Colors.blue),
-                      ),
-                      icon: GFIconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => aboutCOVID19()));
-                        },
-                        icon: Icon(Icons.arrow_forward),
-                      )),
-                  content: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text("Information about the Virus and its Symptoms."),
-                  ),
-                ),
-                GFCard(
-                  boxFit: BoxFit.cover,
-                  color: Colors.white70,
-                  title: GFListTile(
-                      title: Text(
-                        'How can you help prevent it?',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
-                      ),
-                      icon: GFIconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => whatToDo()));
-                        },
-                        icon: Icon(Icons.arrow_forward),
-                      )),
-                  content: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                        "Information about social distancing, quarantine, etc."),
-                  ),
-                ),
-                GFCard(
-                  boxFit: BoxFit.cover,
-                  color: Colors.white70,
-                  title: GFListTile(
-                      title: Text(
-                        'Information about COVID-19 testing',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
-                      ),
-                      icon: GFIconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => CovidTesting()));
-                        },
-                        icon: Icon(Icons.arrow_forward),
-                        //type: GFType.transparent,
-                      )),
-                  content: Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                        "Guidance on where to get tested and who should be tested."),
-                  ),
-                ),
-                GFCard(
-                  boxFit: BoxFit.cover,
-                  color: Colors.white70,
-                  title: GFListTile(
-                      title: Text(
-                        'Other guidelines you should consider',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
-                      ),
-                      icon: GFIconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => otherInformation()));
-                        },
-                        icon: Icon(Icons.arrow_forward),
-                        //type: GFType.transparent,
-                      )),
-                  content:
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8.0),
-                        child: Text("Caring for your body, grocery shopping, work strategy and others."),
-                      ),
-                ),
-              ]
-              // ),
-              ),
+                guidelineCard(
+                    'About COVID-19',
+                    'Information about the Virus and its Symptoms.',
+                    new AboutCovid19()),
+                guidelineCard(
+                    'How can you help prevent it?',
+                    'Information about social distancing, quarantine, etc.',
+                    new whatToDo()),
+                guidelineCard(
+                    'Information about COVID-19 testing',
+                    'Guidance on where to get tested and who should be tested.',
+                    new CovidTesting()),
+                guidelineCard(
+                    'Other guidelines you should consider',
+                    'Caring for your body, grocery shopping, work strategy and others.',
+                    new otherInformation())
+              ]),
         ));
   }
 }
-//
